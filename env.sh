1bb80a74d95159fd59bb7f4d1a470ad43090c740
root <root@bytedance.localdomain>
2024-09-21 01:51:53 +0800

    add env

--- env.sh
+++ env.sh
@@ -1,4 +1,27 @@
 # 依赖包裹
+sudo apt update
+sudo apt install -y build-essential libssl-dev zlib1g-dev libncurses5-dev libncursesw5-dev libreadline-dev libsqlite3-dev libgdbm-dev libdb5.3-dev libbz2-dev libexpat1-dev liblzma-dev tk-dev
+
+cd /usr/src
+sudo wget https://www.python.org/ftp/python/3.8.12/Python-3.8.12.tgz
+sudo tar xzf Python-3.8.12.tgz
+
+cd Python-3.8.12
+sudo ./configure --enable-optimizations
+sudo make altinstall
+
+
+python3.8 --version
+
+
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/local/bin/python3.8 1
+sudo update-alternatives --config python3
+
+sudo apt install python3-pip
+
+pip3 install fastapi uvicorn
+
+
 apt-get install python3-venv
 
 # 创建虚拟环境
