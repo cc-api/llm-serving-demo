ec191356b7194426a91ec903a239022336ee143b
Wenhui Zhang <wenhuizhang.psu@gmail.com>
2024-09-21 18:25:56 -0700

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,3 +1,10 @@
+# 环境
+sudo apt-get install software-properties-common
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt-get install git-lfs
+git lfs install
+git clone git@hf.co:mistralai/Mistral-7B-v0.1
+
 # 依赖包裹
 sudo apt update
 sudo apt install -y build-essential libssl-dev zlib1g-dev libncurses5-dev libncursesw5-dev libreadline-dev libsqlite3-dev libgdbm-dev libdb5.3-dev libbz2-dev libexpat1-dev liblzma-dev tk-dev
